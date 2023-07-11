913d57f28eb7274b04ed28612211dd2a4d8e1c6d
andrey-gbrepose <yandire@yandex.ru>
2023-07-11 17:03:55 +0300

    Zadacha41

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+//больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.WriteLine("Введите c клавиатуры несколько разных чисел через пробел и нажмите 'Enter'");
+string elements = Console.ReadLine();
+Console.WriteLine($"Введен массив из {numArr(elements).Length} чисел. Из них {posQuant(numArr(elements))} положительных.");
+
+
+int posQuant (double[] arr) //Подсчет положительных значений в массиве
+{
+    int i = 0;
+    for (int j = 0; j < arr.Length; j++)
+    {
+      if (arr[j] >= 0) i++;
+    }
+    return i;
+}
+
+double[] numArr (string elem)// Перевод строки в массив double
+{
+    string[] num = elem.Split(" ");
+    double[] sz = new double[num.Length];
+    for (int i =0; i < sz.Length; i++)
+    {
+        sz[i] = Convert.ToDouble(num[i]);
+    }
+    return sz;
+}
